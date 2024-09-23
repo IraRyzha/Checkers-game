c70bcb519db89427592b7d0f6678f402c8606fa7
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-09-23 15:25:38 +0300

    ._.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import "./App.css";
 import Game from "./components/game/game.js";
 import GameProvider from "./context/game-provider.js";
 
-function App() {
+export function App() {
   return (
     <GameProvider>
       <Game />
     </GameProvider>
   );
 }
-
-export default App;
